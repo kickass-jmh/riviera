4ed75e0c68d1017681a19f8b2a3baefc570a0f28
Nenad Bogojevic <nbogojevic@amadeus.com>
2017-04-14 16:21:44 +0200

    Merged with initial

---
+++ src/main/java/fr/rivieradev/jmh/IDontKnowHowToBenchmark.java
@@ -0,0 +1,23 @@
+package fr.rivieradev.jmh;
+
+import java.math.BigInteger;
+import java.util.Random;
+
+public class IDontKnowHowToBenchmark {
+  String x = new BigInteger(8000, new Random()).toString(32);
+  
+  private void method() {
+    x.indexOf("nb");
+  }
+  
+
+  public static void main(String[] args) {
+    IDontKnowHowToBenchmark b = new IDontKnowHowToBenchmark();
+    
+    long instant = System.nanoTime();
+    for (int i = 0; i<1000000000; i++) {
+      b.method();
+    }
+    System.out.println(System.nanoTime() - instant);
+  }
+}
---
+++ src/main/java/fr/rivieradev/jmh/JmhSimpleBenchmark.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2014, Oracle America, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of Oracle nor the names of its contributors may be used
+ *    to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package fr.rivieradev.jmh;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.math.BigInteger;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+@State(Scope.Thread)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+public class JmhSimpleBenchmark {
+
+  String x;
+
+  @Setup
+  public void prepare() {
+    x = new BigInteger(8000, new Random()).toString(32);
+  }
+  
+  @Benchmark
+  public void doNothing() {
+    // do nothing, how fast is a method
+  }
+
+  @Benchmark
+  public void measureWrong() {
+    x.indexOf("aaa");
+  }
+
+  @Benchmark
+  public int measureRight() {
+    return x.indexOf("aaa");
+  }
+
+  public static void main(String[] args) throws RunnerException {
+    Options opt = new OptionsBuilder().include(JmhSimpleBenchmark.class.getSimpleName()).warmupIterations(5)
+                                      .measurementIterations(5).forks(1).build();
+
+    new Runner(opt).run();
+  }
+
+}
