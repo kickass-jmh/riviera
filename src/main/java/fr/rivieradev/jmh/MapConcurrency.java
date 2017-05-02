c0b3d4067c05fc664560cae57c99d1ad00ef2249
Leonardo FREITAS GOMES <leonardo.f.gomes@gmail.com>
2017-05-02 16:56:06 +0200

    Adding profilers

--- src/main/java/fr/rivieradev/jmh/MapConcurrency.java
+++ src/main/java/fr/rivieradev/jmh/MapConcurrency.java
@@ -1,18 +1,30 @@
 package fr.rivieradev.jmh;
 
-import org.openjdk.jmh.annotations.*;
-import org.openjdk.jmh.profile.CompilerProfiler;
-import org.openjdk.jmh.runner.Runner;
-import org.openjdk.jmh.runner.RunnerException;
-import org.openjdk.jmh.runner.options.Options;
-import org.openjdk.jmh.runner.options.OptionsBuilder;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Threads;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.profile.CompilerProfiler;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import profilers.FlightRecordingProfiler;
 
 /**
  * TBD
@@ -66,6 +78,7 @@ public class MapConcurrency {
         Options opt = new OptionsBuilder()
                 .include(MapConcurrency.class.getSimpleName())
                 .addProfiler(CompilerProfiler.class)
+                .addProfiler(FlightRecordingProfiler.class)
                 .build();
 
         new Runner(opt).run();
