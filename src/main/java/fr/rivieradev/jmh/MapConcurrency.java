9bdfdda1f067c4e01ea5fe35efdf42078d285cc5
Leonardo F. Gomes <leonardo.f.gomes@gmail.com>
2017-05-03 22:37:37 +0200

    Concurrency example finished

--- src/main/java/fr/rivieradev/jmh/MapConcurrency.java
+++ src/main/java/fr/rivieradev/jmh/MapConcurrency.java
@@ -18,70 +18,87 @@ import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Threads;
 import org.openjdk.jmh.annotations.Warmup;
-import org.openjdk.jmh.profile.CompilerProfiler;
+import org.openjdk.jmh.infra.Blackhole;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
-import profilers.FlightRecordingProfiler;
-
 /**
- * TBD
+ * <p>
+ * In this benchmark we compare the throughput of a get operation on a
+ * ConcurrentHashMap and a Hashtable. <br>
+ * The ConcurrentHashMap retrievals reflect the results of the most recently
+ * completed update operations holding upon their onset. ConcurrentHashMap get
+ * operation is lock-free. Conversely, Hashtable's get operation uses intrinsic
+ * lock (synchronized).
+ * </p>
+ * 
+ * <p>
+ * The main goal of this benchmark is to show how one can run a multi-threaded
+ * benchmark and explicitly define state scope, i.e. per thread, per benchmark.k
+ * </p>
  */
-@Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
-@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
-@BenchmarkMode(Mode.AverageTime)
-@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@Warmup(iterations = 5, time = 5, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 5, timeUnit = TimeUnit.SECONDS)
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
 @Fork(1)
 public class MapConcurrency {
 
-    @Benchmark
-    @Threads(1)
-    public void hashtable(BenchmarkState bench, ThreadState thread) {
-        for (String s : thread.ids) {
-            bench.hashtable.put(s, s);
-        }
-    }
+	private static final int COUNT = 10;
 
-    @Benchmark
-    @Threads(1)
-    public void concurrentMap(BenchmarkState bench, ThreadState thread) {
-        for (String s : thread.ids) {
-            bench.map.put(s, s);
-        }
-    }
+	@Benchmark
+	@Threads(8)
+	public void hashtable(Blackhole bh, BenchmarkState bench, ThreadState thread) {
+		for (String s : thread.ids) {
+			bh.consume(bench.hashtable.get(s));
+		}
+	}
 
-    @State(Scope.Benchmark)
-    public static class BenchmarkState {
-        Hashtable<String, String> hashtable = new Hashtable<>();
-        ConcurrentHashMap<String, String> map = new ConcurrentHashMap<>();
-    }
+	@Benchmark
+	@Threads(8)
+	public void concurrentMap(Blackhole bh, BenchmarkState bench, ThreadState thread) {
+		for (String s : thread.ids) {
+			bh.consume(bench.map.get(s));
+		}
+	}
 
-    @State(Scope.Thread)
-    public static class ThreadState {
-        private static final int COUNT = 10;
-        private List<String> ids;
+	@State(Scope.Benchmark)
+	public static class BenchmarkState {
 
-        @Setup
-        public void setup() {
-            ids = new ArrayList<>();
-            for (int i = 0; i < COUNT; i++) {
-                String e = "ID" + i;
-                ids.add(e);
-            }
-            Collections.shuffle(ids);
-        }
-    }
+		Hashtable<String, String> hashtable = new Hashtable<>();
+		ConcurrentHashMap<String, String> map = new ConcurrentHashMap<>();
 
-    public static void main(String[] args) throws RunnerException {
-        Options opt = new OptionsBuilder()
-                .include(MapConcurrency.class.getSimpleName())
-                .addProfiler(CompilerProfiler.class)
-                .addProfiler(FlightRecordingProfiler.class)
-                .build();
+		@Setup
+		public void setup() {
+			for (int i = 0; i < COUNT; i++) {
+				String e = "ID" + i;
+				hashtable.put(e, e);
+				map.put(e, e);
+			}
+		}
+	}
 
-        new Runner(opt).run();
-    }
+	@State(Scope.Thread)
+	public static class ThreadState {
+		private List<String> ids;
 
+		@Setup
+		public void setup() {
+			ids = new ArrayList<>();
+			for (int i = 0; i < COUNT; i++) {
+				String e = "ID" + i;
+				ids.add(e);
+			}
+			Collections.shuffle(ids);
+		}
+	}
+
+	public static void main(String[] args) throws RunnerException {
+		Options opt = new OptionsBuilder().include(MapConcurrency.class.getSimpleName())
+				// .addProfiler(StackProfiler.class)
+				.build();
+		new Runner(opt).run();
+	}
 }
